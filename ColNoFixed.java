9ac844a8a2a292a7abeb8e11777cb889b6c887f2
Falak Mohsin <falak.mohsin18@gmail.com>
2026-03-24 20:39:53 +0530

    Add Java DSA codes

---
+++ ColNoFixed.java
@@ -0,0 +1,9 @@
+public class ColNoFixed {
+        public static void main(String[] args) {
+            int[][] arr = {
+                    {1,2,3,4},//0th index
+                    {5,6}, //1st index
+                    {7,8,9} //2nd index
+            }
+    }
+}
